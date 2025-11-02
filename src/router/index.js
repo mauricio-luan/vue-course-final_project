55e0c9e10cb9d7a1ed3f867f51c0c929c2a8788e
Mauricio <mauricioluan2023@gmail.com>
2025-11-02 10:43:28 -0300

    feat: create routes

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,43 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      redirect: '/coaches',
+    },
+    {
+      path: '/coaches',
+      name: 'coaches-list',
+      component: null,
+    },
+    {
+      path: '/coaches/:id',
+      name: 'coach-detail',
+      component: null,
+      children: [
+        {
+          path: 'contact',
+          name: 'coach-contact',
+          component: null,
+        },
+      ],
+    },
+    {
+      path: '/register',
+      name: 'coach-register',
+      component: null,
+    },
+    {
+      path: '/requests',
+      name: 'requests-list',
+      component: null,
+    },
+    {
+      path: '/:notFound(.*)',
+      component: null,
+    },
+  ],
 })
 
 export default router
